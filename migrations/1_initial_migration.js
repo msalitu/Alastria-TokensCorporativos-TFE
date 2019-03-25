3e98db513ccedfc33e235bde5fd5076c1d3a0610
msalitu <mariasalgadoitu@gmail.com>
2019-03-25 18:29:54 +0100

    truffle migrations in alastria

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,3 +3,11 @@ const Migrations = artifacts.require("Migrations");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
+
+// Para Alastria
+/*
+module.exports = function(deployer, network, accounts) {
+  web3.personal.unlockAccount("0x994319e1b1de09aac4aa5b225a7d5cade79d04ed", "Alumnos_2018_Q4_IKx5srvT");
+  deployer.deploy(Migrations, {from:"0x994319e1b1de09aac4aa5b225a7d5cade79d04ed"});
+};
+*/
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,3 +3,9 @@ const PlataformaTokens = artifacts.require("PlataformaTokens");
 module.exports = function(deployer) {
   deployer.deploy(PlataformaTokens);
 };
+
+// Para Alastria
+/*module.exports = function(deployer, network, accounts) {
+  web3.personal.unlockAccount("0x994319e1b1de09aac4aa5b225a7d5cade79d04ed", "Alumnos_2018_Q4_IKx5srvT");
+  deployer.deploy(PlataformaTokens);
+};*/
